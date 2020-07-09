d1f02f8d119e262009fd024b3d210a531c6efe73
Davide Nastri <d.nastri@gmail.com>
2020-07-09 22:57:03 +0200

    feat: Added Sign Up / Authentication

--- src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.c1.review.model;
 
 public class ChatMessage {
+    private Integer messageId;
     private String username;
     private String message;
 
@@ -18,5 +19,13 @@ public class ChatMessage {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Integer messageId) {
+        this.messageId = messageId;
     }
 }
