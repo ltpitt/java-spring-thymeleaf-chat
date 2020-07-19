213862910f7827d5b91d8ab20cbe87124031b677
Davide Nastri <d.nastri@gmail.com>
2020-07-19 22:26:50 +0200

    feat: Working on Selenium tests, added SignUpPage and some tests

--- src/test/java/com/udacity/jwdnd/c1/review/ChatApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/c1/review/ChatApplicationTests.java
@@ -1,11 +1,9 @@
 package com.udacity.jwdnd.c1.review;
 
 import com.udacity.jwdnd.c1.review.page.LoginPage;
+import com.udacity.jwdnd.c1.review.page.SignUpPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,11 +12,22 @@ import org.springframework.boot.web.server.LocalServerPort;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ChatApplicationTests {
     private static WebDriver driver;
     @LocalServerPort
     private Integer port;
     private LoginPage loginPage;
+    private final String username1 = "user1";
+    private final String password1 = "password1";
+    private final String firstName1 = "firstName1";
+    private final String lastName1 = "lastName1";
+    private final String username2 = "user2";
+    private final String password2 = "password2";
+    private final String firstName2 = "firstName2";
+    private final String lastName2 = "lastName2";
+    private SignUpPage signUpPage;
+
 
     @BeforeAll
     public static void beforeAll() {
@@ -33,13 +42,35 @@ class ChatApplicationTests {
 
     @BeforeEach
     public void beforeEach() {
-        driver.get("http://localhost:" + port + "/login");
-        loginPage = new LoginPage(driver);
+
     }
 
     @Test
-    public void testLogin() {
-        loginPage.login("pipi", "pwd");
+    @Order(1)
+    public void testSignUp() throws InterruptedException {
+        driver.get("http://localhost:" + port + "/signup");
+        signUpPage = new SignUpPage(driver);
+        signUpPage.signUp(firstName1, lastName1, username1, password1);
+        assertEquals("You successfully signed up! Please continue to the login page.", signUpPage.getSuccessMessage().getText());
+    }
+
+    @Test
+    @Order(2)
+    public void testLogin() throws InterruptedException {
+        driver.get("http://localhost:" + port + "/login");
+        loginPage = new LoginPage(driver);
+        loginPage.login(username1, password1);
+        Thread.sleep(5000);
+        assertEquals(1, 1);
+    }
+
+    @Test
+    @Order(3)
+    public void testChat() throws InterruptedException {
+        driver.get("http://localhost:" + port + "/chat");
+        loginPage = new LoginPage(driver);
+        loginPage.login(username1, password1);
+        Thread.sleep(5000);
         assertEquals(1, 1);
     }
 
