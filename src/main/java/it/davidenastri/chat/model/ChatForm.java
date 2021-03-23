dbce2313cefd49a68c27703cda7e5de3507cb1e9
Davide Nastri <d.nastri@gmail.com>
2021-03-23 10:32:25 +0100

    feat: Implemented Lombok to simplify and shorten code, solved security issue on log4j dependency

--- src/main/java/it/davidenastri/chat/model/ChatForm.java
+++ src/main/java/it/davidenastri/chat/model/ChatForm.java
@@ -1,31 +1,14 @@
 package it.davidenastri.chat.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ChatForm {
     private String username;
     private String messageText;
     private String messageType;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
-    public String getMessageType() {
-        return messageType;
-    }
-
-    public void setMessageType(String messageType) {
-        this.messageType = messageType;
-    }
 }
--- src/main/java/it/davidenastri/chat/model/ChatMessage.java
+++ src/main/java/it/davidenastri/chat/model/ChatMessage.java
@@ -1,31 +1,12 @@
 package it.davidenastri.chat.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChatMessage {
     private Integer messageId;
     private String username;
     private String messageText;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
-    public Integer getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(Integer messageId) {
-        this.messageId = messageId;
-    }
 }
